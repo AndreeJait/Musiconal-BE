da2a40c6dc809b4cc2a0a298415ba8d1d32e392d
AndreeJait <panjaitanandree@gmail.com>
2021-12-14 00:47:48 +0700

    Update Artikel and Tutorial

--- api/controllers/articleController.js
+++ api/controllers/articleController.js
@@ -4,6 +4,7 @@ const {
   create500Response,
   create200CountResponse,
   create201Response,
+  create200Response,
 } = require("./responseController");
 
 exports.get_all_article = (req, res, next) => {
@@ -19,18 +20,18 @@ exports.get_all_article = (req, res, next) => {
 };
 
 exports.create_new_article = (req, res, next) => {
-  let newArticle = new article({
-    _id: mongoose.Types.ObjectId(),
-    title: req.body.title,
-    content: req.body.content,
-    thubnail: req.file.path,
-    created_by: req.body.created_by,
-  });
-
-  newArticle
-    .save()
-    .then((response) => {
-      create201Response(res, response);
+  article
+    .create({
+      _id: mongoose.Types.ObjectId(),
+      title: req.body.title,
+      content: req.body.content,
+      thubnail: req.file.path,
+      created_by: req.body.created_by,
+    })
+    .then((result) => {
+      result.populate("created_by").then((data) => {
+        create200Response(res, data);
+      });
     })
     .catch((err) => {
       create500Response(res, err);
--- api/controllers/tutorialController.js
+++ api/controllers/tutorialController.js
@@ -3,6 +3,7 @@ const tutorial = require("../models/tutorial");
 const {
   create200CountResponse,
   create500Response,
+  create200Response,
 } = require("./responseController");
 
 exports.get_all_tutorial = (req, res, next) => {
@@ -24,18 +25,19 @@ exports.create_tutorial = (req, res, next) => {
   } else {
     link = req.files.video[0].path;
   }
-  let newTutorial = new tutorial({
-    _id: mongoose.Types.ObjectId(),
-    title: req.body.title,
-    description: req.body.description,
-    thubnail: req.files.thubnail[0].path,
-    link: link,
-    created_by: req.body.created_by,
-  });
-  newTutorial
-    .save()
-    .then((result) => {
-      create200CountResponse(res, result, result.length);
+  tutorial
+    .create({
+      _id: mongoose.Types.ObjectId(),
+      title: req.body.title,
+      description: req.body.description,
+      thubnail: req.files.thubnail[0].path,
+      link: link,
+      created_by: req.body.created_by,
+    })
+    .then((data) => {
+      data.populate("created_by").then((result) => {
+        create200Response(res, result);
+      });
     })
     .catch((err) => {
       create500Response(res, err);
